fad25b74dd0d1002f0a90ab2366ca00ac3037cb0
AhmdFahad <ahmd66699@gmail.com>
2024-05-13 19:28:07 +0300

    implement the getvalue for and save in secure token

--- app/lib.js
+++ app/lib.js
@@ -1,1 +1,9 @@
-export const urlPath = "http://localhost";
+export const urlPath = "http://192.168.100.17:8888";
+export async function save(key, value) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+export async function getValueFor(key) {
+  let result = await SecureStore.getItemAsync(key);
+  return result;
+}
