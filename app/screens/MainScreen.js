00c6878bb53b7fa02033afec842b60f642122e88
AhmdFahad <ahmd66699@gmail.com>
2024-05-17 19:26:38 +0300

    refactor code

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -251,67 +251,6 @@ export default function MainScreen({ navigation }) {
             <PropertyCard key={property.id} property={property} />
           ))}
         </ScrollView>
-        {/* {items.map(({ img, name, price, stars, reviews, saved }, index) => {
-          return (
-            <TouchableOpacity
-              key={index}
-              onPress={() => handleCardPress(items[index])}
-            >
-              <View style={styles.card}>
-                <View style={styles.cardLikeWrapper}>
-                  <TouchableOpacity
-                    onPress={() => {
-                      handleLikePress(index);
-                    }}
-                  >
-                    <View style={styles.cardLike}>
-                      <FontAwesome
-                        color={saved ? "#ea266d" : "#222"}
-                        name="heart"
-                        solid={saved}
-                        size={22}
-                      />
-                    </View>
-                  </TouchableOpacity>
-                </View>
-
-                <View style={styles.cardTop}>
-                  <Image
-                    alt=""
-                    resizeMode="cover"
-                    style={styles.cardImg}
-                    source={{ uri: img }}
-                  />
-                </View>
-
-                <View style={styles.cardBody}>
-                  <View style={styles.cardHeader}>
-                    <Text style={styles.cardTitle}>{name}</Text>
-
-                    <Text style={styles.cardPrice}>
-                      <Text style={{ fontWeight: "600" }}>${price} </Text>/
-                      night
-                    </Text>
-                  </View>
-
-                  <View style={styles.cardFooter}>
-                    <FontAwesome
-                      color="#ea266d"
-                      name="star"
-                      solid={true}
-                      size={12}
-                      style={{ marginBottom: 2 }}
-                    />
-
-                    <Text style={styles.cardStars}>{stars}</Text>
-
-                    <Text style={styles.cardReviews}>({reviews} reviews)</Text>
-                  </View>
-                </View>
-              </View>
-            </TouchableOpacity>
-          );
-        })} */}
       </ScrollView>
       <Toast></Toast>
     </SafeAreaView>
