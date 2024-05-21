8e17ad5ebaad9ddb656cb45ef3c067c124fb19e5
AhmdFahad <AhmdFahad@users.noreply.github.com>
2024-05-21 23:43:36 +0300

    delete post

--- app/screens/PropertyDetailsDashboard.js
+++ app/screens/PropertyDetailsDashboard.js
@@ -15,6 +15,8 @@ import axios from "axios";
 import ImageView from "react-native-image-viewing";
 import LikeSection from "./components/likeSection";
 import { urlPath } from "../lib";
+import { Alert } from "react-native";
+
 import COLORS from "../../assets/Colors/colors";
 import { getValueFor } from "../lib";
 const PropertyDetailsDashboard = ({ route }) => {
@@ -50,143 +52,195 @@ const PropertyDetailsDashboard = ({ route }) => {
 
   const handleSave = async () => {
     try {
-        // Retrieve token asynchronously
-        const token = await getValueFor("token");
+      // Retrieve token asynchronously
+      const token = await getValueFor("token");
 
-        // Check if token is available
-        if (!token) {
-            throw new Error("Token not found");
-        }
+      // Check if token is available
+      if (!token) {
+        throw new Error("Token not found");
+      }
 
-        const propertyId = property.id; // Assuming propertyId is fixed
-        const propertyName = name;
-        const propertyDescription = description;
-        const propertyPrice =parseInt(price);
+      const propertyId = property.id; // Assuming propertyId is fixed
+      const propertyName = name;
+      const propertyDescription = description;
+      const propertyPrice = parseInt(price);
 
-        // Construct the data object using the variables
-        const data = {
-            id: propertyId,
-            name: propertyName,
-            description: propertyDescription,
-            price: propertyPrice
-        };
+      // Construct the data object using the variables
+      const data = {
+        id: propertyId,
+        name: propertyName,
+        description: propertyDescription,
+        price: propertyPrice,
+      };
 
-        const url = urlPath + "/property/";
+      const url = urlPath + "/property/";
 
-        // Make the request with the token included in the headers
-        const response = await axios.put(url, data, {
-            headers: { 
-                'Authorization': `Bearer ${token}`, // Include the token in the Authorization header
-                'Content-Type': 'application/json'
-            }
-        });
+      // Make the request with the token included in the headers
+      const response = await axios.put(url, data, {
+        headers: {
+          Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+          "Content-Type": "application/json",
+        },
+      });
 
-        console.log(JSON.stringify(response.data));
-        // Handle successful response
+      console.log(JSON.stringify(response.data));
+      // Handle successful response
     } catch (error) {
-        console.log(error);
-        // Handle error
+      console.log(error);
+      // Handle error
     }
-};
-  
+  };
+  const handleDelete = async () => {
+    Alert.alert(
+      "Confirm Deletion",
+      "Are you sure you want to delete this item?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Deletion canceled"),
+          style: "cancel",
+        },
+        {
+          text: "OK",
+          onPress: async () => {
+            try {
+              // Retrieve token asynchronously
+              const token = await getValueFor("token");
 
-return (
-  <View style={styles.container}>
-    <ScrollView contentContainerStyle={styles.scrollContent}>
-      <Text style={styles.title}>Edit Property Details</Text>
+              // Check if token is available
+              if (!token) {
+                throw new Error("Token not found");
+              }
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Property Title</Text>
-        <TextInput
-          style={styles.input}
-          value={name}
-          onChangeText={setName}
-          placeholder="Enter property name"
-        />
-      </View>
+              const propertyId = property.id; // Assuming propertyId is fixed
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Property Description</Text>
-        <TextInput
-          style={[styles.input, styles.multilineInput]}
-          value={description}
-          onChangeText={setDescription}
-          placeholder="Enter description"
-          multiline
-        />
-      </View>
+              const url = urlPath + "/property/" + propertyId;
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Property Price</Text>
-        <TextInput
-          style={styles.input}
-          value={price}
-          onChangeText={setPrice}
-          placeholder="Enter price"
-          keyboardType="numeric"
-        />
-      </View>
+              // Make the request with the token included in the headers
+              const response = await axios.delete(url, {
+                headers: {
+                  Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+                  "Content-Type": "application/json",
+                },
+              });
 
-      <TouchableOpacity style={styles.saveButton}
+              console.log(JSON.stringify(response.data));
+              // Handle successful response
+            } catch (error) {
+              console.log(error);
+              // Handle error
+            }
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
 
+  return (
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <Text style={styles.title}>Edit Property Details</Text>
 
-      onPress={handleSave}>
-        <Text style={styles.saveButtonText}>Save</Text>
-      </TouchableOpacity>
-    </ScrollView>
-  </View>
-);
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Property Title</Text>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+            placeholder="Enter property name"
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Property Description</Text>
+          <TextInput
+            style={[styles.input, styles.multilineInput]}
+            value={description}
+            onChangeText={setDescription}
+            placeholder="Enter description"
+            multiline
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Property Price</Text>
+          <TextInput
+            style={styles.input}
+            value={price}
+            onChangeText={setPrice}
+            placeholder="Enter price"
+            keyboardType="numeric"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+          <Text style={styles.saveButtonText}>Save</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.deleteButton} onPress={handleDelete}>
+          <Text style={styles.saveButtonText}>Delete</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: "#fff",
-},
-scrollContent: {
-  paddingBottom: 100,
-},
-title: {
-  fontSize: 28,
-  fontWeight: "bold",
-  marginBottom: 15,
-  marginTop: 70,
-  paddingHorizontal: 20,
-  textAlign: "center",
-  color: COLORS.primary, // Example primary color
-},
-inputContainer: {
-  
-  paddingHorizontal: 20,
-  marginBottom: 10,
-},
-inputLabel: {
-  fontSize: 25,
-  color: COLORS.primary, // Example primary color
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  scrollContent: {
+    paddingBottom: 100,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 15,
+    marginTop: 70,
+    paddingHorizontal: 20,
+    textAlign: "center",
+    color: COLORS.primary, // Example primary color
+  },
+  inputContainer: {
+    paddingHorizontal: 20,
+    marginBottom: 10,
+  },
+  inputLabel: {
+    fontSize: 25,
+    color: COLORS.primary, // Example primary color
 
-  marginBottom: 5,
-},
-input: {
-  borderWidth: 1,
-  borderColor: "#ccc",
-  padding: 10,
-  borderRadius: 5,
-  marginBottom: 15,
-},
-multilineInput: {
-  height: 100,
-},
-saveButton: {
-  backgroundColor: COLORS.primary, // Example primary color
-  padding: 10,
-  borderRadius: 5,
-  marginTop: 20,
-  marginHorizontal: 20,
-},
-saveButtonText: {
-  color: 'white',
-  textAlign: 'center',
-  fontWeight: 'bold',
-},
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 15,
+  },
+  multilineInput: {
+    height: 100,
+  },
+  saveButton: {
+    backgroundColor: COLORS.primary, // Example primary color
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    marginHorizontal: 20,
+  },
+  deleteButton: {
+    backgroundColor: COLORS.darkGray, // Example primary color
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    marginHorizontal: 20,
+  },
+  saveButtonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
 });
 export default PropertyDetailsDashboard;
