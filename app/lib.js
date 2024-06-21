09a39a29da8534a02a7cf13b16477050feebc5ff
AhmdFahad <AhmdFahad@users.noreply.github.com>
2024-06-21 19:59:30 +0300

    update server path

--- app/lib.js
+++ app/lib.js
@@ -1,6 +1,6 @@
 import * as SecureStore from "expo-secure-store";
 
-export const urlPath = `http://ec2-16-24-96-25.me-south-1.compute.amazonaws.com`;
+export const urlPath = `http://15.185.186.31`;
 
 export async function save(key, value) {
   await SecureStore.setItemAsync(key, value);
